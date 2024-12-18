3f62188ed8c1ce79564d9b1b6aa592a99165e35f
Diego Sanchez <diegosanchez.dev018@gmail.com>
2024-12-18 00:56:04 -0600

    Update .gitignore, add streamlink to requirements, and enhance VODs download functionality with improved error handling and status messages

--- vods_automation.py
+++ vods_automation.py
@@ -8,23 +8,51 @@ from extra_functions import get_doc, save_doc
 HEADER = header()
 
 
+def download_vod(vod: dict):
+    url = vod['url']
+    response = get(url, headers=HEADER)
+    if response.status_code == 200:
+        data = response.json()
+        for d in data['data']:
+            download_url = d['url']
+            response = get(download_url)
+            if response.status_code == 200:
+                with open(f'vods/{vod["title"]}.mp4', 'wb') as f:
+                    f.write(response.content)
+                sleep(1)
+                return True
+            else:
+                print(f'Error downloading {vod["title"]}')
+                return False
+    else:
+        print(f'Error downloading {vod["title"]}')
+        return False
+
+
 def download_vods(vods: list):
     for vod in vods:
+        title = vod['title']
+        print(f'Downloading {title}')
+        download_status = download_vod(vod)
+        if download_status == False:
+            print(f'Error downloading {title}')
+            continue
         save_doc(FILENAME, [vod])
-        input('Enter to continue...')
 
 
 def check_vods(vods: list):
     vods_checked = []
     if not path.exists(FILENAME):
-        print('¡NO VODS SAVED!')
+        # print('¡NO VODS SAVED!')
         return vods
     data = get_doc(FILENAME)
-    print(list(data))
     for vod in vods:
-        if vod not in list(data):
-            print(vod)
-        input()
+        for d in list(data):
+            if vod['id'] == d['id']:
+                break
+        else:
+            vods_checked.append(vod)
+    return vods_checked
 
 
 def get_vods(channel_id: int):
@@ -38,11 +66,19 @@ def get_vods(channel_id: int):
 
 def main():
     global FILENAME
+    hours_to_wait = 1
     channel = input('Enter the channel name: ')
     FILENAME = path.join('db', f'{channel}_vods_saved.json')
     channel_id = get_channel_id(channel)
-    vods = get_vods(channel_id)
-    download_vods(vods)
+    while True:
+        vods = get_vods(channel_id)
+        if len(vods) == 0:
+            print('No vods to download!')
+            return
+        print('Vods to download:', len(vods))
+        download_vods(vods)
+        print(f'Waiting {hours_to_wait} hours to check for new vods...')
+        sleep(hours_to_wait * 60 * 60)
 
 
 def vods_automation():
